9bffdb02fb6f219f7a01e3b81978fb530f65d2b1
SabiaSRL <daniele.sabia.dev@hotmail.com>
2025-07-25 13:31:58 +0200

    feat: test setup fixed for CI

--- jumbo-stores-finder/src/test/java/com/jumbo/stores/OpenApiTest.java
+++ jumbo-stores-finder/src/test/java/com/jumbo/stores/OpenApiTest.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(com.jumbo.stores.PostgresContainerConfig.class)
 public class OpenApiTest {
 
     @Autowired
